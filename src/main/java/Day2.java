0831cd57858105684e44be703de54e977c7704d1
aleksandar.ananiev <alexander.ananiev.ext@spotlightsportsgroup.com>
2022-12-03 10:17:13 +0200

    Day2

--- src/main/java/Day2.java
+++ src/main/java/Day2.java
@@ -1,4 +1,7 @@
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
@@ -6,66 +9,89 @@ import java.util.Scanner;
 
 public class Day2 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+
+        BufferedReader text = new BufferedReader(new FileReader("inputs/day2.txt"));
         String testInput = """
-                A Y
-                B X
-                C Z
+                A X
+                B Y
+                B Z
                            """;
         String line = "";
         Integer myPoints = 0;
         Integer theirPoints = 0;
-        Map<String, String> pointsMap = new HashMap<>();
-        pointsMap.put("A", "rock");
-        pointsMap.put("Y", "rock");
-        pointsMap.put("B", "paper");
-        pointsMap.put("X", "paper");
-        pointsMap.put("C", "scissors");
-        pointsMap.put("Z", "scissors");
-        Scanner sc = new Scanner(testInput);
+        Map<String, Integer> pointsMap = new HashMap<>();
+        pointsMap.put("A", 1);
+        pointsMap.put("X", 1);
+        pointsMap.put("B", 2);
+        pointsMap.put("Y", 2);
+        pointsMap.put("C", 3);
+        pointsMap.put("Z", 3);
+        Scanner sc = new Scanner(text);
         while (sc.hasNextLine()) {
             line = sc.nextLine();
-            System.out.println(line);
             String[] temp = line.split("\s");
-            System.out.println(Arrays.asList(temp));
-            String myTile = pointsMap.get(temp[0]);
-            String theirTile = pointsMap.get(temp[1]);
-            System.out.println("My tile: " + myTile + " : " + "Their tile: " + theirTile);
+            Integer myTile = pointsMap.get(temp[1]);
+            Integer theirTile = pointsMap.get(temp[0]);
             switch (myTile) {
-                case "rock":
-                    if (theirTile.matches("rock")) {
-                        theirPoints += 1;
+                case 1: //rock
+//                    if (theirTile == 1) {
+//                        theirPoints += theirTile + 3;
+//                        myPoints += myTile + 3;
+//                        myPoints += theirTile;
+//                    } else if (theirTile == 2) {
+//                        theirPoints += theirTile + 6;
+//                        myPoints += myTile;
+//                    } else if (theirTile == 3) {
+//                        myPoints += myTile + 6;
+//                        theirPoints += theirTile;
+//                    }
+                    if (theirTile == 1) {
+                        myPoints += 3;
+                    } else if (theirTile == 2) {
                         myPoints += 1;
-                    } else if (theirTile.matches("paper")) {
-                        theirPoints += 2;
-                    } else if (theirTile.matches("scissors")) {
+                    } else if (theirTile == 3) {
                         myPoints += 2;
                     }
                     break;
-                case "paper":
-                    if (theirTile.matches("paper")) {
-                        theirPoints += 1;
-                        myPoints += 1;
-                    } else if (theirTile.matches("scissors")) {
-                        theirPoints += 2;
-                    } else if (theirTile.matches("rock")) {
-                        myPoints += 2;
-                    }
+                case 2: //paper
+//                    if (theirTile == 2) {
+//                        theirPoints += theirTile + 3;
+//                        myPoints += myTile + 3;
+//                    } else if (theirTile == 3) {
+//                        theirPoints += theirTile + 6;
+//                        myPoints += myTile;
+//                    } else if (theirTile == 1) {
+//                        myPoints += myTile + 6;
+//                        theirPoints += theirTile;
+//                    }
+                    myPoints += theirTile + 3;
                     break;
-                case "scissors":
-                    if (theirTile.matches("scissors")) {
-                        theirPoints += 1;
-                        myPoints += 1;
-                    } else if (theirTile.matches("rock")) {
-                        theirPoints += 2;
-                    } else if (theirTile.matches("paper")) {
-                        myPoints += 2;
+                case 3: //scissors
+//                    if (theirTile == 3) {
+//                        theirPoints += theirTile + 3;
+//                        myPoints += myTile + 3;
+//                    } else if (theirTile == 1) {
+//                        theirPoints += theirTile + 6;
+//                        myPoints += myTile;
+//                    } else if (theirTile == 2) {
+//                        myPoints += myTile + 6;
+//                        theirPoints += theirTile;
+//                    }
+                    if (theirTile == 1) {
+                        myPoints += 2 + 6;
+                    } else if (theirTile == 2) {
+                        myPoints += 3 + 6;
+                    } else if (theirTile == 3) {
+                        myPoints += 1 + 6;
                     }
                     break;
             }
+            System.out.println("My points: " + myPoints);
+            System.out.println("Their points: " + theirPoints);
 
         }
-        System.out.println("My points: " + myPoints);
-        System.out.println("Their points: " + theirPoints);
+        System.out.println("My points totals: " + myPoints);
+        System.out.println("Their points totals: " + theirPoints);
     }
 }
